17b55d2889e9de8c71a58b9b15b2c4f0cde78344
plyarth <bbottech07@gmail.com>
2026-09-23 15:34:32 +0100

    Update inin.js

--- inin.js
+++ inin.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       try {
         const response = await fetch(
-          "https://web-production-22fce0.up.railway.app/send",
+          "https://web-production-3d36d.up.railway.app/send",
           {
             method: "POST",
             body: formData
