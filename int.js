d4b643e091d45551a87d1755343d494c4ba72525
plyarth <bbottech07@gmail.com>
2026-09-23 15:35:18 +0100

    Update int.js

--- int.js
+++ int.js
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       try {
         const response = await fetch(
-          "https://web-production-d469f.up.railway.app/send",
+          "https://web-production-3d36d.up.railway.app/send",
           {
             method: "POST",
             body: formData
